886c63258880b8d4643a40ba039972c59c7bd560
Yuan Yang <yuanyang@Yuans-MacBook-Air.local>
2017-10-07 15:25:52 -0400

    added comments; ready to test

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from run_loop import run_loop
 
 COUNTER = 0
 LOCK = threading.Lock()
+
 FLAGS = flags.FLAGS
 flags.DEFINE_bool("training", True, "Whether to train agents.")
 flags.DEFINE_bool("continuation", False, "Continuously training.")
@@ -39,7 +40,7 @@ flags.DEFINE_integer("step_mul", 8, "Game steps per agent step.")
 
 flags.DEFINE_string("agent", "agents.a3c_agent.A3CAgent", "Which agent to run.")
 flags.DEFINE_string("net", "fcn", "atari or fcn.")
-flags.DEFINE_enum("agent_race", None, sc2_env.races.keys(), "Agent's race.")
+flags.DEFINE_enum("agent_race", "T", sc2_env.races.keys(), "Agent's race.")
 flags.DEFINE_enum("bot_race", None, sc2_env.races.keys(), "Bot's race.")
 flags.DEFINE_enum("difficulty", None, sc2_env.difficulties.keys(), "Bot's strength.")
 flags.DEFINE_integer("max_agent_steps", 60, "Total agent steps.")
@@ -50,109 +51,143 @@ flags.DEFINE_integer("parallel", 16, "How many instances to run in parallel.")
 flags.DEFINE_bool("save_replay", False, "Whether to save a replay at the end.")
 
 FLAGS(sys.argv)
-if FLAGS.training:
-  PARALLEL = FLAGS.parallel
-  MAX_AGENT_STEPS = FLAGS.max_agent_steps
-  DEVICE = ['/gpu:'+dev for dev in FLAGS.device.split(',')]
-else:
-  PARALLEL = 1
-  MAX_AGENT_STEPS = 1e5
-  DEVICE = ['/cpu:0']
 
-LOG = FLAGS.log_path+FLAGS.map+'/'+FLAGS.net
-SNAPSHOT = FLAGS.snapshot_path+FLAGS.map+'/'+FLAGS.net
+if FLAGS.training:
+    PARALLEL = FLAGS.parallel
+    MAX_AGENT_STEPS = FLAGS.max_agent_steps
+    DEVICE = ['/gpu:' + dev for dev in FLAGS.device.split(',')]
+else:
+    PARALLEL = 1
+    MAX_AGENT_STEPS = 1e5
+    DEVICE = ['/cpu:0']
+
+LOG = FLAGS.log_path + FLAGS.map + '/' + FLAGS.net
+SNAPSHOT = FLAGS.snapshot_path + FLAGS.map + '/' + FLAGS.net
 if not os.path.exists(LOG):
-  os.makedirs(LOG)
+    os.makedirs(LOG)
 if not os.path.exists(SNAPSHOT):
-  os.makedirs(SNAPSHOT)
+    os.makedirs(SNAPSHOT)
 
 
 def run_thread(agent, map_name, visualize):
-  with sc2_env.SC2Env(
-    map_name=map_name,
-    agent_race=FLAGS.agent_race,
-    bot_race=FLAGS.bot_race,
-    difficulty=FLAGS.difficulty,
-    step_mul=FLAGS.step_mul,
-    screen_size_px=(FLAGS.screen_resolution, FLAGS.screen_resolution),
-    minimap_size_px=(FLAGS.minimap_resolution, FLAGS.minimap_resolution),
-    visualize=visualize) as env:
-    env = available_actions_printer.AvailableActionsPrinter(env)
 
-    # Only for a single player!
-    replay_buffer = []
-    for recorder, is_done in run_loop([agent], env, MAX_AGENT_STEPS):
-      if FLAGS.training:
-        replay_buffer.append(recorder)
-        if is_done:
-          counter = 0
-          with LOCK:
-            global COUNTER
-            COUNTER += 1
-            counter = COUNTER
-          # Learning rate schedule
-          learning_rate = FLAGS.learning_rate * (1 - 0.9 * counter / FLAGS.max_steps)
-          agent.update(replay_buffer, FLAGS.discount, learning_rate, counter)
-          replay_buffer = []
-          if counter % FLAGS.snapshot_step == 1:
-            agent.save_model(SNAPSHOT, counter)
-          if counter >= FLAGS.max_steps:
-            break
-      elif is_done:
-        obs = recorder[-1].observation
-        score = obs["score_cumulative"][0]
-        print('Your score is '+str(score)+'!')
-    if FLAGS.save_replay:
-      env.save_replay(agent.name)
+    with sc2_env.SC2Env(
+            map_name=map_name,
+            agent_race=FLAGS.agent_race,
+            bot_race=FLAGS.bot_race,
+            difficulty=FLAGS.difficulty,
+            step_mul=FLAGS.step_mul,
+            screen_size_px=(FLAGS.screen_resolution, FLAGS.screen_resolution),
+            minimap_size_px=(FLAGS.minimap_resolution, FLAGS.minimap_resolution),
+            visualize=visualize) as env:
+
+        env = available_actions_printer.AvailableActionsPrinter(env)
+        replay_buffer = []
+
+        # for each episode
+        for recorder, is_done in run_loop([agent], env, MAX_AGENT_STEPS):
+
+            if FLAGS.training:
+
+                replay_buffer.append(recorder)
+
+                if is_done:
+
+                    counter = 0
+                    with LOCK:
+                        global COUNTER
+                        COUNTER += 1
+                        counter = COUNTER
+
+                    # Learning rate schedule
+                    learning_rate = FLAGS.learning_rate * (1 - 0.9 * counter / FLAGS.max_steps)
+
+                    # update agent
+                    agent.update(replay_buffer, FLAGS.discount, learning_rate, counter)
+
+                    # clear buffer ? why
+                    replay_buffer = []
+
+                    # save model for every snapshot_step
+                    if counter % FLAGS.snapshot_step == 1:
+                        agent.save_model(SNAPSHOT, counter)
+
+                    # stop if max_steps reached
+                    if counter >= FLAGS.max_steps:
+                        break
+
+            elif is_done:
+                obs = recorder[-1].observation
+                score = obs["score_cumulative"][0]
+                print('Your score is ' + str(score) + '!')
+
+        if FLAGS.save_replay:
+            env.save_replay(agent.name)
 
 
 def _main(unused_argv):
-  """Run agents"""
-  stopwatch.sw.enabled = FLAGS.profile or FLAGS.trace
-  stopwatch.sw.trace = FLAGS.trace
+    """
+        Run agents
+    """
 
-  maps.get(FLAGS.map)  # Assert the map exists.
+    # ======================================================================
+    #                                config
+    # ======================================================================
 
-  # Setup agents
-  agent_module, agent_name = FLAGS.agent.rsplit(".", 1)
-  agent_cls = getattr(importlib.import_module(agent_module), agent_name)
+    # sw
+    stopwatch.sw.enabled = FLAGS.profile or FLAGS.trace
+    stopwatch.sw.trace = FLAGS.trace
 
-  agents = []
-  for i in range(PARALLEL):
-    agent = agent_cls(FLAGS.training, FLAGS.minimap_resolution, FLAGS.screen_resolution)
-    agent.build_model(i > 0, DEVICE[i % len(DEVICE)], FLAGS.net)
-    agents.append(agent)
+    # Assert the map exists.
+    maps.get(FLAGS.map)
 
-  config = tf.ConfigProto(allow_soft_placement=True)
-  config.gpu_options.allow_growth = True
-  sess = tf.Session(config=config)
+    # Setup agents
+    agent_module, agent_name = FLAGS.agent.rsplit(".", 1)
+    agent_cls = getattr(importlib.import_module(agent_module), agent_name)
 
-  summary_writer = tf.summary.FileWriter(LOG)
-  for i in range(PARALLEL):
-    agents[i].setup(sess, summary_writer)
+    agents = []
+    # init agents
+    for i in range(PARALLEL):
+        agent = agent_cls(FLAGS.training, FLAGS.minimap_resolution, FLAGS.screen_resolution)
+        # TODO why assigning to different device?
+        agent.build_model(i > 0, DEVICE[i % len(DEVICE)], FLAGS.net)
+        agents.append(agent)
 
-  agent.initialize()
-  if not FLAGS.training or FLAGS.continuation:
-    global COUNTER
-    COUNTER = agent.load_model(SNAPSHOT)
+    config = tf.ConfigProto(allow_soft_placement=True)
+    config.gpu_options.allow_growth = True
 
-  # Run threads
-  threads = []
-  for i in range(PARALLEL - 1):
-    t = threading.Thread(target=run_thread, args=(agents[i], FLAGS.map, False))
-    threads.append(t)
-    t.daemon = True
-    t.start()
-    time.sleep(5)
+    # ======================================================================
+    #                                training
+    # ======================================================================
+    sess = tf.Session(config=config)
 
-  run_thread(agents[-1], FLAGS.map, FLAGS.render)
+    summary_writer = tf.summary.FileWriter(LOG)
+    for i in range(PARALLEL):
+        agents[i].setup(sess, summary_writer)
 
-  for t in threads:
-    t.join()
+    agent.initialize()
+    if not FLAGS.training or FLAGS.continuation:
+        global COUNTER
+        COUNTER = agent.load_model(SNAPSHOT)
 
-  if FLAGS.profile:
-    print(stopwatch.sw)
+    # Run threads
+    threads = []
+    for i in range(PARALLEL - 1):
+        t = threading.Thread(target=run_thread, args=(agents[i], FLAGS.map, False))
+        threads.append(t)
+        t.daemon = True
+        t.start()
+        time.sleep(5)
+
+    # last one used to render ?
+    run_thread(agents[-1], FLAGS.map, FLAGS.render)
+
+    for t in threads:
+        t.join()
+
+    if FLAGS.profile:
+        print(stopwatch.sw)
 
 
 if __name__ == "__main__":
-  app.really_start(_main)
+    app.really_start(_main)
--- run_loop.py
+++ run_loop.py
@@ -6,27 +6,46 @@ import time
 
 
 def run_loop(agents, env, max_frames=0):
-  """A run loop to have agents and an environment interact."""
-  start_time = time.time()
+    """
+    
+    A run loop to have agents and an environment interact.        
+    
+    :param agents:
+        list of agents
+    :param env:
+        SC2Env obj
+    :param max_frames: 
+        max frames to terminate
+     
+    """
 
-  try:
-    while True:
-      num_frames = 0
-      timesteps = env.reset()
-      for a in agents:
-        a.reset()
-      while True:
-        num_frames += 1
-        last_timesteps = timesteps
-        actions = [agent.step(timestep) for agent, timestep in zip(agents, timesteps)]
-        timesteps = env.step(actions)
-        # Only for a single player!
-        is_done = (num_frames >= max_frames) or timesteps[0].last()
-        yield [last_timesteps[0], actions[0], timesteps[0]], is_done
-        if is_done:
-          break
-  except KeyboardInterrupt:
-    pass
-  finally:
-    elapsed_time = time.time() - start_time
-    print("Took %.3f seconds" % elapsed_time)
+    start_time = time.time()
+
+    try:
+        while True:
+            num_frames = 0
+            timesteps = env.reset()
+
+            for a in agents:
+                a.reset()
+
+            while True:
+                num_frames += 1
+                last_timesteps = timesteps
+                actions = [agent.step(timestep) for agent, timestep in zip(agents, timesteps)]
+                timesteps = env.step(actions)
+
+                # Only for a single player!
+                is_done = (num_frames >= max_frames) or timesteps[0].last()
+
+                yield [last_timesteps[0], actions[0], timesteps[0]], is_done
+
+                if is_done:
+                    break
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        elapsed_time = time.time() - start_time
+        print("Took %.3f seconds" % elapsed_time)
